face7b9ae3c0462aa39af8076121493640048638
OFCLeoS <leandroptfr123@outlook.com>
2026-04-16 05:56:22 +0200

    Test Settings

--- src/values/networking_settings.py
+++ src/values/networking_settings.py
@@ -19,12 +19,12 @@ TIME_WITHOUT_PACKETS_BEFORE_PAUSE = 2
 The maximum squared distance allowed for the ball that a client can have from
 the client ball position to the server ball position before a correction is made
 '''
-ALLOWED_SQUARDED_DISTANCE_ERROR = 100
+ALLOWED_SQUARDED_DISTANCE_ERROR = 500
 
 '''
 The time a direction can be difference from the server in a client before a correction is made
 '''
-ALLOWED_DIRECTION_ERROR_TIME  = 0.5
+ALLOWED_DIRECTION_ERROR_TIME  = 1
 
 '''
 The time a client can go without recceiving any packet from the server before an action is taken
